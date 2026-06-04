ad30bf6880e830f20e7573607c4ae44ed38bcbac
benoitguiraudou <benoit@mytime.fr>
2026-06-04 19:43:45 +0200

    fix bug

--- Sources/Photo/CameraPreviewView.swift
+++ Sources/Photo/CameraPreviewView.swift
@@ -8,18 +8,36 @@ public struct CameraPreviewView: UIViewRepresentable {
 		self.session = session
 	}
 
-	public func makeUIView(context: Context) -> UIView {
-		let view = UIView()
+	public func makeUIView(context: Context) -> PreviewContainerView {
+		let view = PreviewContainerView()
 		view.backgroundColor = .black
-		guard let session else { return view }
-		let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-		previewLayer.videoGravity = .resizeAspectFill
-		view.layer.addSublayer(previewLayer)
+		if let session {
+			view.attachSession(session)
+		}
 		return view
 	}
 
-	public func updateUIView(_ uiView: UIView, context: Context) {
-		guard let previewLayer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer else { return }
-		previewLayer.frame = uiView.bounds
+	public func updateUIView(_ uiView: PreviewContainerView, context: Context) {
+		if let session, uiView.previewLayer == nil {
+			uiView.attachSession(session)
+		}
+		uiView.previewLayer?.frame = uiView.bounds
+	}
+}
+
+public final class PreviewContainerView: UIView {
+	var previewLayer: AVCaptureVideoPreviewLayer?
+
+	func attachSession(_ session: AVCaptureSession) {
+		let layer = AVCaptureVideoPreviewLayer(session: session)
+		layer.videoGravity = .resizeAspectFill
+		layer.frame = bounds
+		self.layer.addSublayer(layer)
+		previewLayer = layer
+	}
+
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		previewLayer?.frame = bounds
 	}
 }
